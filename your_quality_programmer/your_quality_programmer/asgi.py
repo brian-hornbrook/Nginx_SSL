8be4823978e02bb3bf6ffabf7ee012e607400d20
Brian Hornbrook <brian_hornbrook@outlook.com>
2026-01-14 16:03:52 -0600

    first commit

---
+++ your_quality_programmer/your_quality_programmer/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for your_quality_programmer project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_quality_programmer.settings')
+
+application = get_asgi_application()
