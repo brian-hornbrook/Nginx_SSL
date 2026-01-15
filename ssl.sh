1e120b3daa089252022c75706ae35b1fb01bce1e
Brian Hornbrook <brian_hornbrook@outlook.com>
2026-01-15 15:19:47 -0600

    set proxy for ssl

--- ssl.sh
+++ ssl.sh
@@ -1,2 +1,4 @@
 ### setup SSL
 docker compose run --rm certbot certonly --webroot --webroot-path=/var/www/certbot --email yourqualityprogrammer@gmail.com --agree-tos --no-eff-email --force-renewal -d yourhonestprogrammer.com -d www.yourhonestprogrammer.com
+
+docker run --rm -v nginx_ssl_certbot_www:/var/www/certbot -v nginx_ssl_certbot_etc:/etc/letsencrypt certbot/certbot certonly --webroot --webroot-path=/var/www/certbot --email yourqualityprogrammer@gmail.com --agree-tos --no-eff-email --force-renewal -d yourhonestprogrammer.com -d www.yourhonestprogrammer.com
